e026ba80e71904a2d98fa0ad3d0c9813f25e318a
rafael-serrado <rafael.serrado@hotmail.com>
2025-08-14 08:24:01 -0300

    aula 14/08

--- Aula 01/aula05.js
+++ Aula 01/aula05.js
@@ -1,1 +1,7 @@
-readline = require("readline-sync")
+console.log('----------------');
+console.log('GERADOR DE PREÇO');
+console.log('----------------');
+readline = require("readline-sync");
+let valor = readline.questionFloat('Qual o valor do produto: ');
+let forma_de_pagamento = readline.questionInt('Forma de pagamento \nDigite 1 para pagamentoa vista \nDigite [2] para pagamento no debito \nDigite [3] para pagamento no credito \nDigite a sua opcao: ');
+console.log(forma_de_pagamento);
