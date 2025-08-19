2191c20640181c4d38d23e2cb8d329a063d7db4b
rafael-serrado <rafael.serrado@hotmail.com>
2025-08-18 21:33:26 -0300

    concluindo-o-primeiro-exercicio

--- Exercício/exercicio01.js
+++ Exercício/exercicio01.js
@@ -1,7 +1,21 @@
-readline = require("readline-sync")
-let real = readline.questionFloat('Digite um valor em Real: ')
+readline = require("readline-sync");
+let real = readline.questionFloat('Digite um valor em Real: ');
 let dolar = real / 0.19
 let euro = real / 0.18
 let dolar_canadense = real / 0.25
 let libra = real / 0.15
-let opcao = readline.question('[1] Para Dólar \n[2] Para Euro \n[3] Para Libra \nDigite uma opção para converter o seu dinheiro: ')
+let opcao = readline.questionInt('[1] Para Dólar \n[2] Para Euro \n[3] Para Libra \n[4] Para Dolar Canadense \nDigite uma opção para converter o seu dinheiro: ');
+switch(opcao){
+    case(1):
+        console.log('Com R$',real, 'Voce pode comprar USD', dolar)
+        break
+    case(2):
+        console.log('Com R$',real, 'Voce pode comprar EUR', euro)
+        break 
+    case(3):
+        console.log('Com R$',real, 'Voce pode comprar GPB', libra)
+        break
+    case(4):
+        console.log('Com R$',real, 'Voce pode comprar USD', dolar_canadense)
+        break
+}
